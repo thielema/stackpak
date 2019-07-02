3a4358f4247d35054e28185695e5248a733555b6
rszibele <richard@szibele.com>
2019-07-02 02:00:36 +0200

    flatpak support

--- build-flatpak.sh
+++ build-flatpak.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-flatpak-builder build-dir/ packaging/Flatpak/com.szibele.stackpak.json
-
+flatpak-builder --force-clean build-dir/ packaging/Flatpak/com.szibele.stackpak.json
+flatpak-builder --repo=repo --force-clean build-dir packaging/Flatpak/com.szibele.stackpak.json
+flatpak build-bundle repo/ stackpak.x86_64.flatpak com.szibele.stackpak --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
